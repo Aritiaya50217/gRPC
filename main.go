475f11572fd5dad173bf5be1df234a87f1a3ad48
artitaya <artitaya2466@gmail.com>
2024-11-05 17:13:17 +0700

    feat : gRPC server

---
+++ main.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"context"
+	"log"
+	"net"
+
+	pb "github.com/Aritiaya50217/gRPC/grpc-hello-world/proto"
+
+	"google.golang.org/grpc"
+)
+
+const (
+	// Port ที่ server
+	port = ":50051"
+)
+
+// struct server ใช้ในการ implement gRPC services
+type server struct {
+	// Embedding UnimplementGreeterServer เพื่อให้มั่นใจว่า struct นี้ implement ทุก method ของ Greeter service
+	pb.UnimplementedGreeterServer
+}
+
+// Implement SayHello method ที่จะตอบกลับคำทักทาย
+// รับ Context และ HelloRequest เป็น input และส่งกลับ HelloReply หรือ error
+func (s *server) SayHello(ctx context.Context, in *pb.HelloRequest) (*pb.HelloReply, error) {
+	// Log ชื่อที่ได้รับจาก request
+	log.Printf("Received: %v", in.GetName())
+
+	// ตอบกลับข้อความทักทาย
+	return &pb.HelloReply{Message: "Hello " + in.GetName()}, nil
+}
+
+func main() {
+	// สร้าง TCP listener ที่ใช้เชื่อมต่อบน port ที่กำหนด
+	lis, err := net.Listen("tcp", port)
+	if err != nil {
+		// ถ้าเกิดข้อผิดพลาดขณะสร้าง listener, ให้ log error แล้วออกจากโปรแกรม
+		log.Fatalf("failed to listen : %v ", err)
+	}
+
+	// สร้าง gRPC server instance ใหม่
+	grpcServer := grpc.NewServer()
+
+	// ลงทะเบียน Greeter service กับ server
+	pb.RegisterGreeterServer(grpcServer, &server{})
+
+	// Log ว่า server กำลังฟังการเชื่อมต่อบน port ที่กำหนด
+	log.Printf("Server is listening on port %v", port)
+
+	// เริ่มฟังการเชื่อมต่อและให้บริการ gRPC request
+	if err := grpcServer.Serve(lis); err != nil {
+		// ถ้าเกิดข้อผิดพลาดขณะให้บริการ, ให้ log error แล้วออกจากโปรแกรม
+		log.Fatalf("failed to serve: %v", err)
+	}
+
+}
